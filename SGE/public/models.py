e91ef93463707a7e38f14ba70af1266eeb2b8136
Jose GUzman <jose.guzman@ucsp.edu.pe>
2020-11-09 15:39:08 -0500

    Base v1.0

---
+++ SGE/public/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+# Create your models here.
+
+"""class Inscripciones(models.Model):
+    id = models.IntegerField(primary_key = True)
+    fecha_inicio = models.DateField(auto_now=False, auto_now_add=False)
+    fecha_cierre = models.DateField(auto_now=False, auto_now_add=False)
+    id_evento = models.ForeignKey(Eventos, on_delete=models.CASCADE)
+
+
+class Preinscripciones(models.Model):
+    id = models.IntegerField(primary_key = True)
+    fecha_inicio = models.DateField(auto_now=False, auto_now_add=False)
+    fecha_cierre = models.DateField(auto_now=False, auto_now_add=False)
+    id_evento = models.ForeignKey(Eventos, on_delete=models.CASCADE)
+
+class Preinscritos(models.Model):
+
+class Inscritos(models.Model):
+
+class Catalogos(models.Model):
+
+class Asistencias(models.Model):
+
+class Egresos(models.Model):
+
+class Ingresos(models.Model):
+
+class Ambientes(models.Model):
+
+class Paquetes(models.Model):
+
+class Eventos(models.Model):
+
+class Comite(models.Model):
+
+class Personal(models.Model):
+
+class Actividades(models.Model):
+
+class Subeventos(models.Model):
+
+class Recibo(models.Model):
+
+class Asistentes(models.Model):"""
+
