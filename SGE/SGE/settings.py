0cd4927806b04cdcfe3da35d7891eaab8b3ce67a
Jose Miguel Guzman <joseguzmanch@hotmail.com>
2020-11-09 18:38:55 -0500

    Change Postgresql

--- SGE/SGE/settings.py
+++ SGE/SGE/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'SGE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'SGE',
+        'USER': 'postgres',
+        'PASSWORD': ' ',#AQUI VA TU CONTRASEÑA DE POSTGRESQL
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
